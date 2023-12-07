57e2e0a16894f0ec15980aa75a07e888c3506ee6
Gregory Pereverzev <boot762@gmail.com>
2023-12-07 22:18:13 +0300

    feat: add base routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { authGuard } from './guards/auth.guard';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
   {
     path: 'signin',
     loadChildren: () => import('./components/login/login.component').then((m) => m.LoginComponent),
@@ -13,13 +12,13 @@ export const routes: Routes = [
     loadChildren: () => import('./components/registration/registration.component').then((m) => m.RegistrationComponent),
   },
   {
-    path: 'profile',
-    loadChildren: () => import('./components/profile/profile.component').then((m) => m.ProfileComponent),
+    path: '',
+    loadChildren: () => import('./components/default/default.component').then((m) => m.DefaultComponent),
     canActivate: [authGuard],
   },
   {
-    path: '',
-    loadChildren: () => import('./components/default/default.component').then((m) => m.DefaultComponent),
+    path: 'profile',
+    loadChildren: () => import('./components/profile/profile.component').then((m) => m.ProfileComponent),
     canActivate: [authGuard],
   },
   {
