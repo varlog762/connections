2ccf50529cfedd5e23db7a3c121fe15eec195bb7
Gregory Pereverzev <boot762@gmail.com>
2023-12-10 01:46:02 +0300

    feat: add base actions for Login

--- src/app/redux/actions/auth.actions.ts
+++ src/app/redux/actions/auth.actions.ts
@@ -1,9 +1,10 @@
 import { createAction, props } from '@ngrx/store';
 
 import { RegistrationRequestIntrface } from '../../models/registration-request.interface';
+import { LoginResponseInterface } from '../../models/login-response.interface';
 
 export const registrationAction = createAction(
-  '[Registration Component] Registration',
+  '[Registration Component] Registration In Progress...',
   props<RegistrationRequestIntrface>(),
 );
 
@@ -27,4 +28,19 @@ export const registrationErrorAction = createAction(
 
 export const regRedirectionSuccessAction = createAction(
   '[registrationSuccessEffects] Do Nothing...',
+);
+
+export const loginAction = createAction(
+  '[Login Component] Login In Progress...',
+  props<RegistrationRequestIntrface>(),
+);
+
+export const loginSuccessAction = createAction(
+  '[Login Component] Login Success',
+  props<{ payload: LoginResponseInterface }>(),
+);
+
+export const loginErrorAction = createAction(
+  '[Login Component] Login Error',
+  props<{ errorType: string; errorMessage: string; email: string | null }>(),
 );
